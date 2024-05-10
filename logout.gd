d5e83d7612202a5c9045b1c9441b4b5178005522
Box_yld <103340124+boxyld@users.noreply.github.com>
2024-05-10 15:11:35 +0300

    Update logout.gd

--- logout.gd
+++ logout.gd
@@ -5,8 +5,6 @@ extends TextureButton
 func _on_pressed():
 	var out = []
 	var user = OS.execute("whoami", [], out)
-	print(out[0].left(-1))
-	var test = []
-	OS.execute("cd",[])
-	OS.execute("cd && ls",[],test)
-	print (test)
+	var player = "pkill -u "+out[0].left(-1)
+	print(player)
+	OS.execute(player,[])
