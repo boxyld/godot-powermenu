ce289a7da0ae17e756558a8a9c8b0440bffb0d63
Box_yld <103340124+boxyld@users.noreply.github.com>
2024-05-10 16:21:19 +0300

    Update logout.gd

--- logout.gd
+++ logout.gd
@@ -3,8 +3,4 @@ extends TextureButton
 
 
 func _on_pressed():
-	var out = []
-	var user = OS.execute("whoami", [], out)
-	var player = "pkill -u "+out[0].left(-1)
-	print(player)
-	OS.execute(player,[])
+	OS.execute("./yld.sh",[])
